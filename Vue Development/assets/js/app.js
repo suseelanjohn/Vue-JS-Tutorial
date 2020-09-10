b15c71ec2edc7794b2cbf6bcd912edee786868a7
suseelanjohn <suseelanjohn@hotmail.com>
2020-09-10 17:50:01 +0530

    Add files via upload

--- Vue Development/assets/js/app.js
+++ Vue Development/assets/js/app.js
@@ -1,19 +1,7 @@
 let app = new Vue({
     el:'#appRoot',
     data:{
-        say:"Hello World"
-    },
-    created:function(){
-        //will work on creation of vue instance 
-        console.log("Vue Created");
-    },
-    mounted:function(){
-        console.log(this.$el.textContent);
-    },
-    updated:function(){
-        console.log("Vue Updated");
-    },
-    destroyed:function(){
-        
+        say:"<h1>Hello Vue</h1>",
+        rawhtml:"<h1>Hello Vue</h1>"
     }
 });
